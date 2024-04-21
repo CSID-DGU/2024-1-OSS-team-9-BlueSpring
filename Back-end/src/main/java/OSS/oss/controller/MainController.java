09e5fa98f928ad74f1f4db453ddd07206c6569dd
sti1andy <116719002+MyungSeo-Kim@users.noreply.github.com>
2024-04-21 18:22:21 +0900

    refactor: 회원가입 로직 완성 및 최적화
    - 회원가입 관련 코드 주석 추가
    - SecurityConfig 경로 인가, 권한 수정
    - API 명세에 맞게 URL 매핑 조정
    - react에서 api 요청 전달 위한 proxy 설정 추가(package.json)

--- Back-end/src/main/java/OSS/oss/controller/MainController.java
+++ Back-end/src/main/java/OSS/oss/controller/MainController.java
@@ -2,9 +2,11 @@ package OSS.oss.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/api")
 public class MainController {
     @GetMapping("/test")
     @ResponseBody
