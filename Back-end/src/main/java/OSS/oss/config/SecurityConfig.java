9d75f87f8518e3b7ff084aff8529c0c60fe2435d
sti1andy <116719002+MyungSeo-Kim@users.noreply.github.com>
2024-04-28 15:45:45 +0900

    feat: 로그인 로직 구현
    - 스프링 시큐리티 이용한 기본 로그인 로직 추가
    - 아이디, 비밀번호 검증을 위한 커스텀 필터 작성
    - authenticManager로 DB 기반 로그인 검증 로직 완성
    - 로그인 성공 시 JWT 반환하는 success 핸들러 생성

--- Back-end/src/main/java/OSS/oss/config/SecurityConfig.java
+++ Back-end/src/main/java/OSS/oss/config/SecurityConfig.java
@@ -1,16 +1,31 @@
 package OSS.oss.config;
 
+import OSS.oss.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    //AuthenticationConfiguration 객체를 변수 선언
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+    //AuthenticationManager 객체를 bean 등록해 생성자 방식으로 주입
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
     @Bean
     // 비밀번호 암호화
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -34,8 +49,13 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/**", "/resister").permitAll() // login, /, resister 경로는 누구나 접근 가능
-                        //.requestMatchers("/admin").hasRole("ADMIN") // ADMIN 권한이 있어야 접근 가능
+                        .requestMatchers("/admin").hasRole("ADMIN") // ADMIN 권한이 있어야 접근 가능
                         .anyRequest().authenticated());
+
+        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
         //세션 설정 -> 가장 중요
         http
                 .sessionManagement((session) -> session
